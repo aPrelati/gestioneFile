92d3e8a2de7998d7b170f6308e8abb0df98eb2cc
aPrelati <alessioprelati04@gmail.com>
2022-02-14 17:50:15 +0100

    situazione iniziale

---
+++ app/src/main/java/com/example/gestionefile/Gestore.java
@@ -0,0 +1,66 @@
+package com.example.gestionefile;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.nio.charset.StandardCharsets;
+
+public class Gestore
+{
+
+    String nomeFile;
+
+    public  String leggiFile(String nomeFile, Context c)
+    {
+        StringBuilder sb= new StringBuilder();
+        try
+        {
+            BufferedReader fileIn= new BufferedReader(new InputStreamReader(c.openFileInput(nomeFile)));
+            String text="";
+            while((text= fileIn.readLine())!= null){
+                sb.append(text+"\n");
+            }
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            Log.e("classe Gestore", "il file non esiste");
+        } catch (IOException e)
+        {
+            Log.e("classe Gestore", "impossibile leggere il file");
+        }
+        return sb.toString();
+    }
+
+    public String scriviFile(String nomeFile, Context c)
+    {
+        String esito;
+        FileOutputStream fileOutput;
+        String textWrite= "Testo da scrivere";
+        try
+        {
+            fileOutput= c.openFileOutput(nomeFile, Context.MODE_PRIVATE);
+            fileOutput.write(textWrite.getBytes());
+            fileOutput.close();
+            esito="File scritto correttamente";
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            Log.e("classe Gestore", "impossibile leggere il file in SCRITTURA");
+            esito= "File non trovato";
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            Log.e("classe Gestore", "impossibile scivere sul file");
+            esito= "Impossibile scrivere sul file";
+        }
+
+        return esito;
+    }
+
+}
---
+++ app/src/main/java/com/example/gestionefile/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.gestionefile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLeggi;
+    Button btnScrivi;
+    TextView fileLetto;
+    EditText nomeFile;
+    Gestore gest= new Gestore();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLeggi= findViewById(R.id.leggi);
+        btnScrivi= findViewById(R.id.scrivi);
+        fileLetto= findViewById(R.id.visualizzaFile);
+        nomeFile= findViewById(R.id.nomeFile);
+
+        //String righeLette= gest.leggiFile("prova.txt", getApplicationContext());
+        //Toast.makeText(getApplicationContext(), righeLette, Toast.LENGTH_LONG).show();
+
+        btnScrivi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String esito= gest.scriviFile("prova.txt", getApplicationContext());
+                Toast.makeText(getApplicationContext(), esito, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnLeggi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
